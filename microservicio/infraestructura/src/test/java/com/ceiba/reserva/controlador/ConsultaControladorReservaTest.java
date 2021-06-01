d9b6f6de4b370c1cabfe56ba85b403ba88472cca
diegoferose <diegoferose@hotmail.com>
2021-06-01 08:52:05 -0500

    se crea prueba de integracion

---
+++ microservicio/infraestructura/src/test/java/com/ceiba/reserva/controlador/ConsultaControladorReservaTest.java
@@ -0,0 +1,49 @@
+package com.ceiba.reserva.controlador;
+
+import com.ceiba.ApplicationMock;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.core.Is.is;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes= ApplicationMock.class)
+@WebMvcTest(ConsultaControladorReserva.class)
+public class ConsultaControladorReservaTest {
+    @Autowired
+    private MockMvc mocMvc;
+
+    @Test
+    public void listar() throws Exception {
+        // arrange
+
+        // act - assert
+        mocMvc.perform(get("/reservas")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].identificacionUsuario", is("123")));
+    }
+
+    @Test
+    public void listarPorIdentificacion() throws Exception {
+        // arrange
+        String id = "123";
+        // act - assert
+        mocMvc.perform(get("/reservas",id)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].identificacionUsuario", is("123")));
+    }
+
+}
